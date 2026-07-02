adde5f88970119204896a250f08580d0591a1eae
dakshajinu007-maker <dakshajinu007@gmail.com>
2026-07-02 19:08:20 +0530

    Ransome note (hash map) #383

---
+++ Ransom_note.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d1={}
+        d2={}
+        for i in ransomNote:
+            if i in d1:
+                d1[i]=d1[i]+1
+            else:
+                d1[i]=1
+        for i in magazine:
+            if i in d2:
+                d2[i]=d2[i]+1
+            else:
+                d2[i]=1
+        for i in d1:
+            if i not in d2:
+                return False
+            if d2[i]<d1[i]:
+                return False
+        return True
