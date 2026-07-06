ca52b72845d2606c1072c9f95b4fd5215905e2e6
dakshajinu007-maker <dakshajinu007@gmail.com>
2026-07-06 23:29:59 +0530

    remove duplicates (2-pointers) #26

---
+++ remove_duplicates.py
@@ -0,0 +1,8 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pos=0
+        for i in range(1,len(nums)):
+            if nums[i]!=nums[pos]:
+                pos+=1
+                nums[pos]=nums[i]
+        return pos+1
